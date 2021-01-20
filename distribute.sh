e0bd3106a2c6d5873e479d8c3d470773a6406e75
eggersn <st160727@stud.uni-stuttgart.de>
2021-01-20 18:07:11 +0100

    fabricstar & distribution script

---
+++ distribute.sh
@@ -0,0 +1,12 @@
+# /bin/bash
+
+echo "Make sure you have a ssh-agent running!"
+echo
+
+iplist="ip-list.txt"
+
+while IFS= read -r line 
+do
+    echo "$line"
+    ssh "ubuntu@$line" -f "rm -rf caliper-benchmarks; git clone https://github.com/eggersn/caliper-benchmarks"
+done < "$iplist"
