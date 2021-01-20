49134076ae92b818b496f09d247c545994f60171
eggersn <st160727@stud.uni-stuttgart.de>
2021-01-20 18:12:33 +0100

    modified to make repo public

--- distribute.sh
+++ distribute.sh
@@ -8,5 +8,5 @@ iplist="ip-list.txt"
 while IFS= read -r line 
 do
     echo "$line"
-    ssh "ubuntu@$line" -f "rm -rf caliper-benchmarks; git clone https://github.com/eggersn/caliper-benchmarks"
+    ssh "ubuntu@$line" -f "rm -rf caliper-benchmarks; git clone https://github.com/eggersn/fabricstar-benchmarks"
 done < "$iplist"
